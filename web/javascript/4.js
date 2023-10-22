df12c943d9b356d809d64efbde50635e6545a78f
John-Gan <neo_decade@outlook.com>
2023-10-22 15:51:37 +1000

    "web html"

---
+++ web/javascript/4.js
@@ -0,0 +1,13 @@
+function selectWeek(evt, WeekNum){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i< tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(WeekNum).style.display = "block";
+    evt.currentTarget.className += " active";
+}
